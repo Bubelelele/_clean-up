49de078d5fb14772499df9f7f30ec688c3653e5a
Boarario <79580336+Boarario@users.noreply.github.com>
2021-11-04 11:37:40 +0100

    Bug fixes.
    Fixed not being able to move after replaying on death, and being able to clean after death.

--- Assets/_Scripts/MainMenu.cs
+++ Assets/_Scripts/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene("BuildingRotation");
+        PaperMoving.onScreen = false;
         PlayerHealth.isDead = false;
         ScoreController.score = 0;
         PauseMenu.GameIsPaused = false;
