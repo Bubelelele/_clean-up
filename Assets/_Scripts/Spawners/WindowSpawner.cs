7461e21c732ee7fd8b264a771193f840dd97e38c
Boarario <79580336+Boarario@users.noreply.github.com>
2021-11-19 11:53:32 +0100

    WIP Window Spawner

---
+++ Assets/_Scripts/Spawners/WindowSpawner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowSpawner : MonoBehaviour
+{
+    private float laneOffset = 2.1f, spawnerOffset = 5.31f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnWindows()
+    {
+
+    }
+}
