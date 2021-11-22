fc820dafd2ef552f0bea6096ad639fc26f4f8c85
Bubelelele <89357128+Bubelelele@users.noreply.github.com>
2021-11-22 14:28:17 +0100

    Changed the colours of the chairs

--- Assets/_Scripts/ChairColor.cs
+++ Assets/_Scripts/ChairColor.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ChairColor : MonoBehaviour
 {
 
-    public Material green, red, yellow, blue, black;
+    public Material black, blue, crimson, yellow;
 
     private Material[] materialList;
     private Material thisMaterial;
@@ -16,26 +16,22 @@ public class ChairColor : MonoBehaviour
     {
         materialList = gameObject.GetComponent<MeshRenderer>().materials; //Takes all the materials on the chair object and puts them in a list.
         
-        colorpicker = Random.Range(1,6); //Chooses a random color.
+        colorpicker = Random.Range(1,5); //Chooses a random color.
         if (colorpicker == 1)
         {
-            thisMaterial = green;
+            thisMaterial = black;
         }
         if (colorpicker == 2)
         {
-            thisMaterial = red;
+            thisMaterial = blue;
         }
         if (colorpicker == 3)
         {
-            thisMaterial = yellow;
+            thisMaterial = crimson;
         }
         if (colorpicker == 4)
         {
-            thisMaterial = blue;
-        }
-        if (colorpicker == 5)
-        {
-            thisMaterial = black;
+            thisMaterial = yellow;
         }
         
         materialList[2] = thisMaterial; //Finds the seat material in the list and changes it to the randomly selected color material.
