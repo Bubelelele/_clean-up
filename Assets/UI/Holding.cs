3c84032380efce05596c039df35728d54375c21f
Bubelelele <89357128+Bubelelele@users.noreply.github.com>
2021-11-23 18:17:41 +0100

    Updated holding.cs to use relative screen size

--- Assets/UI/Holding.cs
+++ Assets/UI/Holding.cs
@@ -4,10 +4,10 @@ using UnityEngine.SceneManagement;
 public class Holding : MonoBehaviour
 {
     private Animator holding;
-    private float holdRangeXmin = 105f;
-    private float holdRangeXmax = 170f;
-    private float holdRangeYmin = 145f;
-    private float holdRangeYmax = 225f;
+    private float holdRangeXmin = Screen.width * 0.38f;
+    private float holdRangeXmax = Screen.width * 0.62f;
+    private float holdRangeYmin = Screen.height * 0.29f;
+    private float holdRangeYmax = Screen.height * 0.45f;
 
     public GameObject arrow_up, arrow_down;
 
@@ -46,20 +46,20 @@ public class Holding : MonoBehaviour
                 holding.SetBool("Holding", false);
                 transform.position = new Vector3(139f, 185f, 1f);
 
-                holdRangeXmin = 105f;
-                holdRangeXmax = 170f;
-                holdRangeYmin = 145f;
-                holdRangeYmax = 225f;
+                holdRangeXmin = Screen.width * 0.38f;
+                holdRangeXmax = Screen.width * 0.62f;
+                holdRangeYmin = Screen.height * 0.29f;
+                holdRangeYmax = Screen.height * 0.45f;
 
                 arrow_up.SetActive(false);
                 arrow_down.SetActive(false);
             }
 
-        if (holding.GetBool("Holding") && Input.mousePosition.y > 300f)
+        if (holding.GetBool("Holding") && Input.mousePosition.y > Screen.height * 0.6f)
         {
             SceneManager.LoadScene("BuildingRotationMain");
         }
-        else if (holding.GetBool("Holding") && Input.mousePosition.y < 40f)
+        else if (holding.GetBool("Holding") && Input.mousePosition.y < Screen.height * 0.08f)
         {
             SceneManager.LoadScene("Leaderboard from Menu");
         }
