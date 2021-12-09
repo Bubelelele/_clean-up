9043c36a61bb6834e9acfea6f4a7fa409c774da6
Boarario <79580336+Boarario@users.noreply.github.com>
2021-12-09 10:54:23 +0100

    Death animation clean-up.
    Made death animations use newly added character time. Objects don't freeze around you during any death animation now, which makes them better in my opinion.

--- Assets/_Scripts/ScrollingTexture.cs
+++ Assets/_Scripts/ScrollingTexture.cs
@@ -26,7 +26,7 @@ public class ScrollingTexture : MonoBehaviour
         offset = (int)quadRenderer.material.mainTextureOffset.y;
         if (AnimationManager.isDead == true)
         {
-            MasterTime.masterTime = 0f;
+            MasterTime.characterTime = 0f;
             heightTotal = ((int)quadRenderer.material.mainTextureOffset.y).ToString() + "m";
             lbHeight = ((int)quadRenderer.material.mainTextureOffset.y);
             PlayerPrefs.SetInt("height", lbHeight);
