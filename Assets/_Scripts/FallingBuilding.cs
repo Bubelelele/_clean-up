da84da4d7ba2415dc6618045ca0b28d3496c12e6
Boarario <349606@student.nord.no>
2022-02-24 11:05:50 +0100

    Skybox, fog, emission.
    +Added skybox changing at 500 and 1000. Still need to add transition clouds.
    +Made buildings use mastertime so they all consistently fall at 500 despite speeding up with mastertime control keys.
    +Added emission to chairs.
    +Added slight fog so distant buildings look better.

--- Assets/_Scripts/FallingBuilding.cs
+++ Assets/_Scripts/FallingBuilding.cs
@@ -6,6 +6,11 @@ public class FallingBuilding : MonoBehaviour
 {
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y -1 * Time.deltaTime, transform.position.z);
+        transform.position = new Vector3(transform.position.x, transform.position.y -1 * MasterTime.masterTime * Time.deltaTime, transform.position.z);
+
+        if(transform.position.y < -200)
+        {
+            Destroy(gameObject);
+        }
     }
 }
