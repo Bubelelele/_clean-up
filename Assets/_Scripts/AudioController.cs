6102810da6646bebc6443deba6ef8a5b14057758
Boarario <349606@student.nord.no>
2022-02-28 16:42:01 +0100

    Update AudioController.cs

--- Assets/_Scripts/AudioController.cs
+++ Assets/_Scripts/AudioController.cs
@@ -20,16 +20,28 @@ public class AudioController : MonoBehaviour
         janitorRockstar = pjanitorRockstar;
         gameLoop = pgameLoop;
 
-        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("MenuScene") && !menuMusic.isPlaying)
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("MenuScene"))
         {
-            menuMusic.Play();
             gameLoop.Stop();
+            if(!menuMusic.isPlaying)
+            {
+                menuMusic.Play();
+            }
         }
 
         if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("BuildingRotationMainTesting") && !gameLoop.isPlaying)
         {
             menuMusic.Stop();
             gameLoop.Play();
+        }
+
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("GameLeaderboardScene") || SceneManager.GetActiveScene() == SceneManager.GetSceneByName("MenuLeaderboardScene"))
+        {
+            if(!menuMusic.isPlaying)
+            {
+                menuMusic.Play();
+            }
+            gameLoop.Stop();
         }
 
         if (AnimationManager.isDead == true || JanitorRockstar.rockstarActivated == true)
